c9f2a5787cf2f2b3ee7fa356e827aba8f11ccfa0
Sandy <90114858+Sandy4521@users.noreply.github.com>
2023-05-02 12:33:09 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -162,9 +162,9 @@ ScrollReveal().reveal('.home .info h2, .section-title-01, .section-title-02',{ d
 ScrollReveal().reveal('.home .info h3, .home .info p, .about-info .btn', { delay: 600, origin: 'right'});
 ScrollReveal().reveal('.home .info .btn',{delay: 700, origin: 'bottom'});
 ScrollReveal().reveal('.media-icons i, .contact-left li',{delay: 500, origin: 'left', interval: 200});
-ScrollReveal().reveal('.home-img, about-img', {delay: 500, origin: 'bottom'});
+ScrollReveal().reveal('.home-img, .about-img', {delay: 500, origin: 'bottom'});
 ScrollReveal().reveal('.about .description, .copy-right', {delay: 600, origin: 'right'});
 ScrollReveal().reveal('.about .profesional-list li', {delay: 500, origin: 'right', interval:200});
-ScrollReveal().reveal('.skills-description, service-description, .contact-card, .client-swiper, contact-left h2', {delay: 700, origin: 'left' });
+ScrollReveal().reveal('.skills-description, .services-description, .contact-card, .client-swiper, contact-left h2', {delay: 700, origin: 'left' });
 ScrollReveal().reveal('.experience-card, .service-card, .education, .portfolio .img-card', {delay: 800, origin: 'bottom', interval:200});
 ScrollReveal().reveal('footer .group', {delay: 500, origin: 'top', interval:200});
